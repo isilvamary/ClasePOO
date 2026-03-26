43e7e6b11450f22a2d425e3dfb831915145531f7
Harry34343 <hkligman@uninorte.edu.co>
2026-03-26 14:06:43 +0000

    a

--- src/clasepoo/Libro/Audiolibro.java
+++ src/clasepoo/Libro/Audiolibro.java
@@ -1,10 +1,29 @@
-public class Audiolibro extends Libro{
-    private int duracion;
-    private Narrador Narrador;
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package core.book;
 
-    public Audiolibro(String titulo, ArrayList<Autor> autores, String ISBN, String genero, String formato, float valor, Editorial Editorial, int duracion, Narrador Narrador){
-        super(titulo, autores, ISBN, genero, formato, valor, Editorial);
+import core.megaferia.Editorial;
+import core.person.Autor;
+import core.person.Narrador;
+import java.util.ArrayList;
+
+/**
+ *
+ * @author castillodj
+ */
+public class Audiolibro extends Libro {
+    
+    private int duracion;
+    private Narrador narrador;
+
+    public Audiolibro(String titulo, ArrayList<Autor> autores, String isbn, String genero, String formato, double valor, Editorial editorial, int duracion, Narrador narrador) {
+        super(titulo, autores, isbn, genero, formato, valor, editorial);
         this.duracion = duracion;
-        this.Narrador = Narrador;
+        this.narrador = narrador;
+        
+        this.narrador.addLibro(this);
     }
+    
 }
