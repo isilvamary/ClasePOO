64e2ca7ace88e96bdb6442b7906401c86a44c947
isilvamary <isilvam@uninorte.edu.co>
2026-03-26 13:46:49 +0000

    Se añadió main de referencia y clase megaferia para la creación de métodos

--- src/clasepoo/Megaferia.java
+++ src/clasepoo/Megaferia.java
@@ -1,24 +1,3 @@
-/*
- * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
- * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
- */
-package clasepoo;
-
-/**
- *
- * @author isilvam
- */
 public class Megaferia {
 
-    /**
-     * @param args the command line arguments
-     */
-    public static void main(String[] args) {
-        
-        
-        
-        
-        
-    }
-    
 }
